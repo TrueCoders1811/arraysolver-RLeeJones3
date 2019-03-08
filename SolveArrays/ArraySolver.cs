ec3cc624d029fdae6d3ea508f009f169a8242430
Rjones <rjonessoft@gmail.com>
2019-03-07 20:44:48 -0600

    Completed the last two problems.

--- SolveArrays/ArraySolver.cs
+++ SolveArrays/ArraySolver.cs
@@ -33,14 +33,39 @@ namespace SolveArrays
 
         public double AverageEvens(int[] numbers)
         {
-            // TODO: Implement this method
-            throw new NotImplementedException();
+            double total = 0;
+            double count = 0;
+            foreach (int num in numbers)
+            {
+                if (num %2 == 0)
+                {
+                    total += num;
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                return 0; 
+            }
+            return total / count;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            // TODO: Implement this method
-            throw new NotImplementedException();
+            int total = 0;
+            foreach (int num in numbers)
+            {
+                total += num;
+            }
+            if (total % 2 != 0)
+            {
+                return true; 
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
