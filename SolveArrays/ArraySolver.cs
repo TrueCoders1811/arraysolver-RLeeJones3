77dc914fa0ff2e93a65f6413e18a2d9399ded8b1
Rjones <rjonessoft@gmail.com>
2019-03-07 20:13:45 -0600

    Did first two array problems

--- SolveArrays/ArraySolver.cs
+++ SolveArrays/ArraySolver.cs
@@ -7,14 +7,28 @@ namespace SolveArrays
     {
         public int Sum(int[] numbers)
         {
-            // TODO: Implement this method
-            throw new NotImplementedException();
+            int total = 0;
+           foreach (int num in numbers)
+            {
+                total += num;
+            }
+
+            return total;
         }
 
         public int SumEvens(int[] numbers)
         {
-            // TODO: Implement this method
-            throw new NotImplementedException();
+            int total = 0;
+            foreach (int num in numbers)
+            {
+                if (num %2 == 0)
+                {
+                    total += num;
+                }
+            }
+
+            return total;
+
         }
 
         public double AverageEvens(int[] numbers)
